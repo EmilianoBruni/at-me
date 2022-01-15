2a2cb6a9605b6f1e28e9d4d178e572b537377437
ebruni <info@ebruni.it>
2022-01-15 13:47:52 +0100

    Support for shock advise, push button request and shock cancelled if too much time as gone.

--- sw/arduino/at-me/src/Aed.cpp
+++ sw/arduino/at-me/src/Aed.cpp
@@ -2,57 +2,80 @@
 #include <DFRobotDFPlayerMini.h>
 
 Aed::Aed(DFRobotDFPlayerMini player, byte pin_power_led, byte pin_pads, byte pin_pads_led, byte pin_shock, byte pin_shock_led) {
-        this->player        = player;
-        this->pin_power_led = pin_power_led;
-        this->pin_pads      = pin_pads;
-        this->pin_pads_led  = pin_pads_led;
-        this->pin_shock     = pin_shock;
-        this->pin_shock_led = pin_shock_led;
-        setup();
-    }
+    this->player        = player;
+    this->pin_power_led = pin_power_led;
+    this->pin_pads      = pin_pads;
+    this->pin_pads_led  = pin_pads_led;
+    this->pin_shock     = pin_shock;
+    this->pin_shock_led = pin_shock_led;
+    setup();
+}
 
-    void Aed::setup() {
-        pinMode(pin_power_led, OUTPUT);
-        pinMode(pin_pads, INPUT);
-        pinMode(pin_shock, INPUT_PULLUP);
-        pinMode(pin_pads_led, OUTPUT);
-        pinMode(pin_shock_led, OUTPUT);
-        powerOff();
-    }
+void Aed::setup() {
+    pinMode(pin_power_led, OUTPUT);
+    pinMode(pin_pads, INPUT);
+    pinMode(pin_shock, INPUT_PULLUP);
+    pinMode(pin_pads_led, OUTPUT);
+    pinMode(pin_shock_led, OUTPUT);
+    powerOff();
+}
 
-    void Aed::setShockLed(bool to_on) {
-        digitalWrite(pin_shock_led, to_on ? HIGH : LOW);
-    }
+void Aed::setShockLed(bool to_on) {
+    digitalWrite(pin_shock_led, to_on ? HIGH : LOW);
+}
 
-    void Aed::loop() {
-        bool pp = digitalRead(pin_pads);
-        if ( pp == HIGH && state != PowerOff && state < PadsConnected) {
-            setState(PadsConnected);
-        } else if ( pp == LOW && state > PadsNotConnected) {
-            setState(PadsNotConnected);
-        }
-        this->togglePadsLed();
-        if (player.available()) {
-            //Print message from DFPlayer to handle different errors states.
-            checkPlayerStatus(player.readType(), player.read());
-            //printDetail(mPlayer.readType(), mPlayer.read());
-    }
-    // DEBUG: and then, when finished
-    if (playFinishedId == SND_BEEP) {
-        playFinishedId = SND_UNDEF;
+void Aed::loop() {
+    flowControl();
+    this->togglePadsLed();                       // here because it's a timer
+                                                 // loop
+    if (player.available()) checkPlayerStatus(player.readType(), player.read());
+}
+
+void Aed::flowControl() {
+    // here all aed flowchart logic
+    static unsigned int  delayPushButton = 8000; // delay to play push button
+
+    static unsigned long analizingTimer  = 0;    // timer for analise
+    static unsigned long shockTimer      = 0;    // timer for shock
+    static unsigned long pushButtonTimer = 0;    // timer if operator does'nt
+                                                 // play shock
+    // manage linking pads
+    bool                 pp              = digitalRead(pin_pads);
+    if ( pp == HIGH && state != PowerOff && state < PadsConnected) {
+        setState(PadsConnected);
+        if (analizingTimer == 0) analizingTimer = millis();
+    } else if ( pp == LOW && state > PadsNotConnected) {
         setState(PadsNotConnected);
+        analizingTimer = 0;
     }
-    if (playFinishedId == SND_APPLY_PADS) { //SND_BEEP) {
-        playFinishedId = SND_UNDEF;
 
+    // analizing timer
+    if (analizingTimer>0 && millis() - analizingTimer>8000) {
+        analizingTimer  = 0;
+        delayPushButton = 8000;
+        setState(ShockRequired); // or not, but for now always yes
+        shockTimer      = millis();
+    }
 
-        play(SND_LINK_PADS);
+    // shock timer every x seconds
+    if (shockTimer > 0 && millis() - shockTimer > delayPushButton ) {
+        shockTimer      = millis();
+        setState(PushButton, delayPushButton != 8000);
+        delayPushButton = 15000;
+        if (pushButtonTimer == 0) pushButtonTimer = millis();
+    }
+
+    //pushButtonTimer
+    if (pushButtonTimer>0 && millis() - pushButtonTimer > 40000) {
+        shockTimer      = 0;
+        pushButtonTimer = 0;
+        setState(ShockCancelled);
     }
 }
 
 void Aed::play(byte id) {
     playFinishedId = SND_UNDEF;
-    playStartId=id;
+    playStartId    = id;
     player.playFolder(getLang(), id);
     Serial.print(F("Start playing "));
     Serial.println(id);
@@ -71,13 +94,14 @@ void Aed::powerOff() {
     setState(PowerOff);
 }
 
-void Aed::setState(int state) {
+void Aed::setState(int state, int opt = 0) {
     Serial.println("setState " + String(state) + " lang: " + String(getLang()));
     this->state = state;
     switch (state) {
     case PoweredOn:
         play(SND_BEEP);
-        // if powerof with pads connected don't play SND_ANALYZING without this patch
+        // if powerof with pads connected don't play SND_ANALYZING without this
+        // patch
         if (digitalRead(pin_pads) == HIGH) delay(100);
         break;
     case PadsNotConnected:
@@ -85,6 +109,18 @@ void Aed::setState(int state) {
         break;
     case PadsConnected:
         play(SND_ANALYZING);
+        break;
+    case ShockRequired:
+        play(SND_SHOCK_ADVISED);
+        break;
+    case PushButton:
+        opt == 0 ? play(SND_PUSH_BUTTON) : play(SND_PUSH_BUTTON_AGAIN);
+        break;
+    case ShockCancelled:
+        play(SND_SHOCK_CANCEL);
+        break;
+    case InPause:
+        play(SND_IN_PAUSE);
         break;
     default:
         break;
@@ -118,8 +154,28 @@ void Aed::checkPlayerStatus(uint8_t type, int value){
         Serial.print(F(" - Folder id:"));
         Serial.print(playFinishedId);
         Serial.println(F(" Play Finished!"));
+        checkPlayNext();
         break;
     }
+}
+
+void Aed::checkPlayNext() {
+    // check if there is something to play when finished an mp3
+    switch (playFinishedId) {
+    case SND_BEEP:
+        setState(PadsNotConnected);
+        break;
+    case SND_APPLY_PADS:
+        play(SND_LINK_PADS);
+        break;
+    case SND_PUSH_BUTTON:
+    case SND_PUSH_BUTTON_AGAIN:
+        play(SND_LONG_BEEP);
+        break;
+    case SND_SHOCK_CANCEL:
+        setState(InPause);
+    }
+    playFinishedId = SND_UNDEF;
 }
 
 int Aed::getState() {
--- sw/arduino/at-me/src/Aed.h
+++ sw/arduino/at-me/src/Aed.h
@@ -4,22 +4,31 @@
 #include <Arduino.h>
 #include <DFRobotDFPlayerMini.h>
 
-#define PowerOff 0
-#define PoweredOn        1
-#define PadsNotConnected 2
-#define PadsConnected    3
-#define Analyzing        4
-#define ShockNotRequired 5
-#define ShockRequired    6
-#define ShockDelivered   7
-#define InPause          99
+#define PowerOff              0
+#define PoweredOn             1
+#define PadsNotConnected      2
+#define PadsConnected         3
+#define Analyzing             4
+#define ShockNotRequired      5
+#define ShockRequired         6
+#define PushButton            7
+#define ShockDelivered        8
+#define ShockCancelled        9
+#define InPause               99
 
-#define SND_UNDEF        -1
-#define SND_LANG_IT      1
-#define SND_BEEP         2
-#define SND_APPLY_PADS   10
-#define SND_LINK_PADS    11
-#define SND_ANALYZING    12
+#define SND_UNDEF             -1
+#define SND_LANG_IT           1
+#define SND_BEEP              2
+#define SND_APPLY_PADS        10
+#define SND_LINK_PADS         11
+#define SND_ANALYZING         12
+#define SND_SHOCK_ADVISED     14
+#define SND_PUSH_BUTTON       15
+#define SND_LONG_BEEP         16
+#define SND_PUSH_BUTTON_AGAIN 17
+#define SND_SHOCK_DELIVERED   18
+#define SND_SHOCK_CANCEL      19
+#define SND_IN_PAUSE          99
 
 
 
@@ -42,7 +51,10 @@ void checkPlayerStatus(uint8_t type, int value);
 void setup();
 void play(byte id);
 void setShockLed(bool to_on);
-void setState(int state);
+void setState(int state, int opt = 0);
+void checkPlayNext();
+void flowControl();
+
 
 public:
 // Setup pin LED and call init()
