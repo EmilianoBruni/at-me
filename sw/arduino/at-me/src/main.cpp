5cf4d9f970ead266c54db282c29c9f887375d5c1
Emiliano Bruni <info@ebruni.it>
2022-12-10 00:09:19 +0100

    Change Button and Led Ids to better organize PCB

--- sw/arduino/at-me/src/main.cpp
+++ sw/arduino/at-me/src/main.cpp
@@ -11,13 +11,13 @@
 #define PADS_ID            3
 #define SHOCK_BUTTON_ID    4
 #define SHOCK_LED_ID       5
-#define POWER_BUTTON_ID    6
-#define SW_SERIAL_RX_ID    10
-#define SW_SERIAL_TX_ID    11
+#define POWER_BUTTON_ID    7
+#define POWER_LED_ID       10
+#define SW_SERIAL_RX_ID    11
+#define SW_SERIAL_TX_ID    12
 
 #define VOLUME_VALUE_INIT  20  // 0..30
 #define POWER_BUTTON_DELAY 500 // press powerButton for 0.5s
-#define POWER_LED_ID       LED_BUILTIN
 //const int           DEBUG_LED_ID       = LED_BUILTIN;
 
 
