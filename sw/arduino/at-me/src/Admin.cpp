f8f84b205147c59f0055a76c0862ad9a3c0a3ef6
Emiliano Bruni <info@ebruni.it>
2023-01-17 23:41:28 +0100

    Add skel class used to configure aed

---
+++ sw/arduino/at-me/src/Admin.cpp
@@ -0,0 +1,41 @@
+#include "Admin.h"
+
+Admin::Admin(byte lang)
+{
+    this->lang = lang;
+    setup();
+}
+
+Admin::~Admin()
+{
+}
+
+void Admin::setup() 
+{
+    this->powerOff();
+}
+
+void Admin::loop()
+{
+}
+
+void Admin::powerOn()
+{
+    setState(PoweredOn);
+}
+
+void Admin::powerOff()
+{
+    setState(PowerOff);
+}
+
+void Admin::setState(int state)
+{
+    Serial.println("Admin::setState " + String(state) + " lang: " + String(this->lang));
+    this->state = state;
+}
+
+int Admin::getState()
+{
+    return state;
+}
---
+++ sw/arduino/at-me/src/Admin.h
@@ -0,0 +1,26 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include <Arduino.h>
+
+#define Invalid -1
+#define PowerOff 0
+#define PoweredOn 1
+
+class Admin
+{
+private:
+    int state;
+    byte lang;
+    void setup();
+public:
+    Admin(byte lang);
+    ~Admin();
+    void powerOn();
+    void powerOff();
+    void loop();
+    void setState(int state);
+    int getState();
+};
+
+#endif
