79002c4f67e67c44767b923a05ee0f8ef04ecd1c
Emiliano Bruni <info@ebruni.it>
2024-05-19 22:16:25 +0200

    feat: Add volume control functionality to Admin class

--- sw/arduino/at-me/src/Admin.cpp
+++ sw/arduino/at-me/src/Admin.cpp
@@ -1,8 +1,9 @@
 #include "Admin.h"
 
-Admin::Admin(byte lang)
+Admin::Admin(byte lang, DFRobotDFPlayerMini player)
 {
     this->lang = lang;
+    this->player = player;
     setup();
 }
 
@@ -38,4 +39,31 @@ void Admin::setState(int state)
 int Admin::getState()
 {
     return state;
+}
+
+void Admin::volumeUp()
+{
+    int volume = this->player.readVolume();
+    Serial.println("Admin::volumeUp " + String(volume));
+    if (volume < 30) {
+        this->player.volume(volume + 5);
+        delay(50);  // to permit volume change and received serial response to this command
+        this->player.playFolder(1, 98);
+    } else {
+        this->player.playFolder(1, 98);
+        delay(150);  // to permit volume change and received serial response to this command
+        this->player.playFolder(1, 98);
+    }
+}
+
+void Admin::volumeDown()
+{   
+    int volume = this->player.readVolume();
+    Serial.println("Admin::volumeDown " + String(volume));
+    if (volume > 0) {
+        this->player.volume(volume - 5);
+        delay(50);  // to permit volume change and received serial response to this command
+        this->player.playFolder(1, 98);
+    }
+
 }
--- sw/arduino/at-me/src/Admin.h
+++ sw/arduino/at-me/src/Admin.h
@@ -2,6 +2,7 @@
 #define ADMIN_H
 
 #include <Arduino.h>
+#include <DFRobotDFPlayerMini.h>
 
 #define Invalid -1
 #define PowerOff 0
@@ -13,14 +14,17 @@ private:
     int state;
     byte lang;
     void setup();
+    DFRobotDFPlayerMini player;
 public:
-    Admin(byte lang);
+    Admin(byte lang, DFRobotDFPlayerMini player);
     ~Admin();
     void powerOn();
     void powerOff();
     void loop();
     void setState(int state);
     int getState();
+    void volumeUp();
+    void volumeDown();
 };
 
 #endif
